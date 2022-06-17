a16bc7cb3cce19fbccbea7bd819b4b2d71f28bc5
Vitaly Nam <vitaliknam01@gmail.com>
2022-06-17 19:17:07 +0300

    Добавлены модалки для картинок в галерее

---
+++ js/popup.js
@@ -0,0 +1,26 @@
+const popupBtn = document.querySelectorAll('.popup-click'); // элемент по которому произведен клик
+const popupItem = document.querySelectorAll('.popup__img'); // основной контент внутри попапа
+const popupClose = document.querySelector('.popup__close'); // кнопка закрытия попапа
+const popupWrap = document.querySelector('.popup'); // фон (оверлей) попапа
+
+if(popupBtn.length === popupItem.length){
+    for(let i = 0; i<popupBtn.length; i++){
+
+        popupBtn[i].onclick = () =>{
+            document.body.classList.add('_lock');
+            popupWrap.classList.add('active');
+            popupItem[i].classList.add('active');
+        }
+
+    }
+}else{
+    console.log('Количество кликабельных элементов не равно количеству элементов в попапе')
+}
+
+popupClose.onclick = () =>{
+    document.body.classList.remove('_lock');
+    popupWrap.classList.remove('active');
+    for (const elem of popupItem) {
+        elem.classList.remove('active');        
+    }
+}
